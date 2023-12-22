170a82532402ef26cd098857cd2acbab667a8d87
Bakhmeteva Yulya <bakhmetevayulya@gmail.com>
2023-12-22 17:05:14 +0200

    douSearch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,20 @@
 package org.example;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://mvnrepository.com/");
-        WebElement input = driver.findElement(By.xpath("//input[@id='query']"));
-        input.sendKeys("Hello");
+        driver.get("https://dou.ua/");
+        WebElement input = driver.findElement(By.xpath(".//input[@id = 'txtGlobalSearch']"));
+        input.sendKeys("Alevel");
+        input.sendKeys(Keys.ENTER);
         driver.quit();
     }
 }
