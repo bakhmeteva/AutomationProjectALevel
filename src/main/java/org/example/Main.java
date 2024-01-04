0d7fc97921e9d879c289eb0a867eb7afb0e20a15
Bakhmeteva Yulya <bakhmetevayulya@gmail.com>
2024-01-04 18:39:59 +0200

    OptimizeImports

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
